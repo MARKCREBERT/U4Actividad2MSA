b06e91be630090f9592ec780e0b4d6f4d0e18f87
Marcos Sepulcre <marcos2161999@gmail.com>
2019-11-26 15:45:22 +0100

    Creación de proyecto

---
+++ src/u4actividad2msa/U4Actividad2MSA.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package u4actividad2msa;
+
+/**
+ *
+ * @author ciclost
+ */
+public class U4Actividad2MSA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        U4Actividad2MSA programa = new U4Actividad2MSA();
+        programa.inicio();
+    }
+    
+    public void inicio() {
+       
+        
+        
+    }
+    
+    
+    
+}
+/*taza clic derecho, versioing, initialize git repository, solo crear el nuevco proyecto
+, das ok, las letras se pondrán en verde, eso es que está en modo stage, clic derecho en el proyecto-> git
+add -> los avances que vayamos realizando en la Actividad 4.2, dijo algo del ckecout(creo)
+luego habrá que enviarle el enlace del github y no un archivo.java
+*/
