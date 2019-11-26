2ce95a08b0ee2af5890230e6819d7699b1e6d7d8
Marcos Sepulcre <marcos2161999@gmail.com>
2019-11-26 16:54:23 +0100

    Métodos escritos, y paloAleatorio editado y diagrama añadido

--- src/u4actividad2msa/U4Actividad2MSA.java
+++ src/u4actividad2msa/U4Actividad2MSA.java
@@ -21,10 +21,72 @@ public class U4Actividad2MSA {
     
     public void inicio() {
        
-        // comentario
+        paloAleatorioDominante();
+        
         
     }
     
+    public char paloAleatorioDominante() { 
+
+       // creación aleatoria entre el 1 y 4, y cada número le correspondrá un palo
+  
+       int paloAleatorio = (int) (Math.random()*4)+49;  
+       //char sirve para guardar los caracteres de la tabla ASCII
+       char paloElegido = (char) paloAleatorio;
+        
+        
+        if (paloElegido == '1') {
+            
+            System.out.println("Palo dominante => Bastos");
+            
+        } else if (paloElegido == '2') {
+            
+            System.out.println("Palo dominante => Espadas");
+            
+        } else if (paloElegido == '3') {
+            
+            System.out.println("Palo dominante => Oros");
+            
+        } else if (paloElegido == '4') {
+            
+            System.out.println("Palo dominante => Copas");
+        }
+        
+       return paloElegido;
+
+    }
+  
+    
+    
+    public String repartirCartas() {     
+        
+        return null;
+
+    }
+    
+    public String palos() {
+        
+        
+        return null;
+    }
+    
+    public String eleccionJugador() {
+        
+        
+        return null;
+    }
+    
+    public String visionCartasDelJugador() {
+        
+        
+        return null;
+    }
+    
+    public String ganadorDeLaPartida() {
+        
+        
+        return null;
+    }
     
     
 }
@@ -32,4 +94,39 @@ public class U4Actividad2MSA {
 , das ok, las letras se pondrán en verde, eso es que está en modo stage, clic derecho en el proyecto-> git
 add -> los avances que vayamos realizando en la Actividad 4.2, dijo algo del ckecout(creo)
 luego habrá que enviarle el enlace del github y no un archivo.java
+
+click derecho en la taza o en el .java, commit
+
+click derecho, giff, giff to HEAD (para ver como lo tenías anteriormente)
+*/
+
+/*
+
+public int mayor (int a, int b) { // poner dos returns, se puede hacer, pero el primer return que coja, devolverá la respuesta el método mayor y finalizará
+
+    if (a < b) {
+        return a;
+    }
+    else if (b > a) {
+        return b;
+    }
+}
+*/
+
+/*
+
+al final, cuando ya lo tenemos todo hecho, lo enviamos al repositorio Remoto que cree: llamado Actividad2MSA
+
+*/
+
+
+/*
+
+PARA IMPORTAR PROYECTO Y ASÍ NO DA ERRORES EN CADA AL COMITEAR Y TAL
+
+Me pongo encima de la taza del proyecto a exportar
+File -> Export y la guardas en el escritorio y en el nombre del archivo, actividad4.zip (importante ponerle .zip)
+
+En casa FIle -> Import
+
 */
