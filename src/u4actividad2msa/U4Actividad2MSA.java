64f9687774f86b1fd038af7d87661e370743f95a
Marcos Sepulcre <marcos2161999@gmail.com>
2019-12-03 15:44:34 +0100

    añado la muestra de cartas que tiene cada jugador

--- src/u4actividad2msa/U4Actividad2MSA.java
+++ src/u4actividad2msa/U4Actividad2MSA.java
@@ -20,121 +20,284 @@ public class U4Actividad2MSA {
         U4Actividad2MSA programa = new U4Actividad2MSA();
         programa.inicio();
     }
-    
-    public void inicio() {
-        
-        paloAleatorioDominante();
-        
-        int [][] baraja = new int [4][12];
-        
-        repartirCartas(baraja, 1); // poniendo el 1, llamamos al jugador 1
-        
-        
-        
-        
-    }
-    
-    public char paloAleatorioDominante() { 
 
-       // creación aleatoria entre el 1 y 4, y cada número le correspondrá un palo
-  
-       int paloAleatorio = (int) (Math.random()*4)+49;  
-       //char sirve para guardar los caracteres de la tabla ASCII
-       char paloElegido = (char) paloAleatorio;
+    public void inicio() {
+
+        paloAleatorioDominante();
+
+        int[][] baraja = new int[4][12];
+        String[][] cartas = new String[4][4];
+        // si tengo que borrar esto, hacer captura y explicarmelo
+        baraja = repartirCartas();
+        /*  poniendo baraja, nos rellenar el array 
+                                    bidimensional con los valores que se tomen en el metodo, repartir cartas, corresponiente a su
+                                    parametro que en este caso sería->  repartirCartas(int baraja[][])*/
+        palos(baraja);
+
+        cartas = recorrerBaraja(baraja);
+        
+        System.out.println("\n");
+        
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+
+                System.out.print(cartas[i][j]);
+
+            }
+            System.out.print("\n");
+        }
         
         
+
+    }
+
+    public char paloAleatorioDominante() {
+
+        // creación aleatoria entre el 1 y 4, y cada número le correspondrá un palo
+        int paloAleatorio = (int) (Math.random() * 4) + 49;
+        //char sirve para guardar los caracteres de la tabla ASCII
+        char paloElegido = (char) paloAleatorio;
+
         if (paloElegido == '1') {
-            
+
             System.out.println("Palo dominante => Bastos");
-            
+
         } else if (paloElegido == '2') {
-            
+
             System.out.println("Palo dominante => Espadas");
-            
+
         } else if (paloElegido == '3') {
-            
+
             System.out.println("Palo dominante => Oros");
-            
+
         } else if (paloElegido == '4') {
-            
+
             System.out.println("Palo dominante => Copas");
         }
         
         
-       return paloElegido;
+
+        return paloElegido;
 
     }
-  
-    
-    
-    public int[][] repartirCartas(int baraja[][], int numJugador) {   
-        
+
+    public int[][] repartirCartas() {
+
         Random random = new Random(); // random sería un objeto
-        
-        int filas = 0;
-        int columnas = 0;
-        int jugador = numJugador;
-        
-        
-        int [][] colFil = new int [4][12]; // así llenamos el array
-        
-        for (int j = 0; j < 4; j++) {
-            
-        for (int i = 0; i < 4; i++) {
-              
-               filas = random.nextInt(4);  // crea valores enteros entre el 0 y el 4
-               columnas = random.nextInt(12); // crea valores enteros entre el 0 y el 11
-               
-               // este if es para que no chafe un numero a otro
-               if(colFil[filas][columnas] == 0) {
-               colFil[filas][columnas] = j+1; //para que en cada iteracion sume un jugador, porque sino j empieza en 0, y no debe haber un jugador que sea 0
-               }
-               else{i--;}// ccuando no sea 0 se va hacia atrás para compensar la carta que no ha puesto
-               
-               
-        }}
-        
+
+        int filas;
+        int columnas;
+
+        int[][] colFil = new int[4][12]; // así llenamos el array, 4 jugadores X 12 figuras distintas a repartir entre cada jugador
+
+        for (int j = 0; j < 4; j++) { // 4 es porque debe repartir 4 cartas a cada jugador
+
+            for (int i = 0; i < 4; i++) {
+
+                filas = random.nextInt(4);  // crea valores enteros del 0 al 3
+                columnas = random.nextInt(12); // crea valores enteros del 0 al 11
+
+                // este if es para que no chafe un numero a otro
+                if (colFil[filas][columnas] == 0) { // si en la fila x y la columna x hay un 0 que entre y se le asigne el j+1
+                    colFil[filas][columnas] = j + 1; // esto suma el jugador para la siguiente iteracion en el bucle j, una vez ya se le han repartido las 4 cartas al anterior jugador
+                } else {
+                    i--; // si en fila x y la columna x ya hay un valor asignado (ya sea 1, 2, 3, 4), que reste una iteracion
+                }
+
+            }
+        }
+        // aqui se va mostrando el valor que tiene cada posicion del array bidimensional ya almacenado
+        // y así al final aparecerán las 4 filas X las 12 columnas con sus 4 cartas divididas a cada jugador
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 12; j++) {
-                
+
                 System.out.print(colFil[i][j]);
-                
+
             }
-            
+
             System.out.println("");
         }
-        
-        // ahora sería asignar el valor en otro método, por ejemplo asignar valor a las cartas, que busque posicion, si hay un 0 que asigne, sino no
-        
+
+        System.out.println("");
+
         return colFil;
 
     }
-    
-    public String palos() {
-        
-        
-        return null;
+
+    // este método no sirve, solo es para visualizarlo
+    public void palos(int[][] baraja) {
+
+        System.out.print("Bastos -> ");
+
+        // Este bucle saca la primera linea de colFil
+        for (int k = 0; k < 1; k++) {
+            for (int l = 0; l < 12; l++) {
+
+                int bastos[] = new int[12];
+
+                bastos[l] = baraja[k][l];
+
+                System.out.print(bastos[l]);
+            }
+
+            System.out.println("");
+        }
+
+        System.out.print("Espadas -> ");
+        // Este bucle saca la segunda linea de colFil
+        for (int k = 1; k < 2; k++) {
+            for (int l = 0; l < 12; l++) {
+
+                int espadas[] = new int[12];
+
+                espadas[l] = baraja[k][l];
+
+                System.out.print(espadas[l]);
+            }
+
+            System.out.println("");
+        }
+
+        System.out.print("Oros -> ");
+        // Este bucle saca la tercera linea de colFil
+        for (int k = 2; k < 3; k++) {
+            for (int l = 0; l < 12; l++) {
+
+                int oros[] = new int[12];
+
+                oros[l] = baraja[k][l];
+
+                System.out.print(oros[l]);
+            }
+
+            System.out.println("");
+        }
+        System.out.print("Copas -> ");
+        // Este bucle saca la cuarta linea de colFil
+        for (int k = 3; k < 4; k++) {
+            for (int l = 0; l < 12; l++) {
+
+                int copas[] = new int[12];
+
+                copas[l] = baraja[k][l];
+
+                System.out.print(copas[l]);
+            }
+
+            System.out.println("");
+        }
+
     }
-    
+
+    public String[][] recorrerBaraja(int baraja[][]) {
+
+        int t = 0;
+        String cartas[][] = new String[4][4];
+
+        for (int m = 0; m < 4; m++) {// jugador
+            t=0;
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < 12; j++) {
+
+                    if (baraja[i][j] == m + 1) {
+                        if (t < 4) {
+                            cartas[m][t] = codificar(i, j);
+
+                            t++;
+                        }
+                    }
+
+                }
+
+            }
+        }
+
+        return cartas;
+    }
+
+    public String codificar(int i, int j) {
+
+        String carta = "";
+
+        if (j < 9 && j != 0) {
+
+            carta = String.valueOf(j + 1); // asi convertimos los enteros a String para luego concatenarlos
+        }
+
+        switch (j) {
+
+            case 1: // el AS
+
+                carta = "A";
+
+                break;
+
+            case 9: // sota
+
+                carta = "S";
+
+                break;
+
+            case 10: // caballo
+
+                carta = "C";
+
+                break;
+
+            case 11: //rey
+
+                carta = "R";
+
+                break;
+
+        }
+
+        switch (i) {
+
+            case 0: // Oros
+
+                carta = carta.concat("0");
+
+                break;
+
+            case 1: // espadas
+
+                carta = carta.concat("E");
+
+                break;
+
+            case 2: // bastos
+
+                carta = carta.concat("B");
+
+                break;
+
+            case 3: //copas
+
+                carta = carta.concat("C");
+
+                break;
+
+        }
+
+        return carta;
+    }
+
     public String eleccionJugador() {
-        
-        
+
         return null;
     }
-    
+
     public String visionCartasDelJugador() {
-        
-        
+
         return null;
     }
-    
+
     public String ganadorDeLaPartida() {
-        
-        
+
         return null;
     }
-    
-    
+
 }
 /*taza clic derecho, versioing, initialize git repository, solo crear el nuevco proyecto
 , das ok, las letras se pondrán en verde, eso es que está en modo stage, clic derecho en el proyecto-> git
@@ -144,9 +307,9 @@ luego habrá que enviarle el enlace del github y no un archivo.java
 click derecho en la taza o en el .java, commit
 
 click derecho, giff, giff to HEAD (para ver como lo tenías anteriormente)
-*/
+ */
 
-/*
+ /*
 
 public int mayor (int a, int b) { // poner dos returns, se puede hacer, pero el primer return que coja, devolverá la respuesta el método mayor y finalizará
 
@@ -157,16 +320,14 @@ public int mayor (int a, int b) { // poner dos returns, se puede hacer, pero el 
         return b;
     }
 }
-*/
+ */
 
-/*
+ /*
 
 al final, cuando ya lo tenemos todo hecho, lo enviamos al repositorio Remoto que cree: llamado Actividad2MSA
 
-*/
-
-
-/*
+ */
+ /*
 
 PARA IMPORTAR PROYECTO Y ASÍ NO DA ERRORES EN CADA AL COMITEAR Y TAL
 
@@ -175,4 +336,7 @@ File -> Export y la guardas en el escritorio y en el nombre del archivo, activid
 
 En casa FIle -> Import
 
-*/
+ */
+
+
+/*remote-> git push y en casa git pull*/
